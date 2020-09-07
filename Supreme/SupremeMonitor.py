06f004b266878e97bfe5a7be69a132a0fddcd3e7
Yasser Qureshi <yasser.q@hotmail.com>
2020-09-07 22:57:25 +0100

    fixing discord

--- Supreme/SupremeMonitor.py
+++ Supreme/SupremeMonitor.py
@@ -18,7 +18,7 @@ class SupremeMonitor:
     def discord_webhook(self, product_item):
         # product_item = [title, colour, image link, link, sizes]
         description = ''
-        for i in range(len(product_item[1])):
+        for i in range(len(product_item[4])):
             if i % 2 == 1:
                 description = description + str(product_item[4][i].replace(' : ', '/')) + '\n'
             else:
@@ -26,7 +26,7 @@ class SupremeMonitor:
 
         data = {}
         data["username"] = "Supreme Monitor"
-        data["avatar_url"] = '[insert image url]'
+        #data["avatar_url"] = 'image url'
         data["embeds"] = []
         embed = {}
         embed["title"] = product_item[0] + ' - ' + product_item[1]               # Item Name
@@ -35,8 +35,9 @@ class SupremeMonitor:
         embed["color"] = 15258703
         embed["thumbnail"] = {'url': product_item[2]}                            # Item image
         embed["footer"] = {'text': 'Made by Yasser Qureshi'}
-        embed["timestamp"] = str(datetime.datetime.now())
+        #embed["timestamp"] = str(datetime.datetime.now())
         data["embeds"].append(embed)
+        print(data)
 
         result = rq.post(self.webhook, data=json.dumps(data), headers={"Content-Type": "application/json"})
 
@@ -52,21 +53,21 @@ class SupremeMonitor:
         s = rq.Session()
         try:
             html = s.get('https://www.supremenewyork.com/mobile_stock.json', headers=self.headers, verify=False,
-                         timeout=1)
+                         timeout=3)
             output = json.loads(html.text)['products_and_categories']
             self.pages.append(output)
-        except:
-            print('There was an Error')
+        except Exception as e:
+            print('There was an Error - main site - ', e)
 
     def scrape_item_site(self, name, id):
         # product_item = [title, colour, image link, link, sizes]
         try:
             url = 'https://www.supremenewyork.com/shop/' + str(id) + '.json'
-            html = rq.get(url, headers=self.headers, verify=False, timeout=0.3)
+            html = rq.get(url, headers=self.headers, verify=False, timeout=3)
             output = json.loads(html.text)['styles']
 
             for colour in output:
-                instock = [name, colour['name'], 'https' + colour['image_url'], url.split('.json')[0], []]
+                instock = [name, colour['name'], 'https:' + colour['image_url'], url.split('.json')[0], []]
                 for size in colour['sizes']:
                     if size['stock_level'] == 1:
                         if self.checker(name, colour['name'], size['name']):
@@ -83,10 +84,8 @@ class SupremeMonitor:
                         pass
                     else:
                         self.discord_webhook(instock)
-                        print(instock)
-            time.sleep(3)
-        except:
-            print('There was an Error')
+        except Exception as e:
+            print('There was an Error - single site - ', e)
 
     def checker(self, product, colour, size):
         # check if item is in list
@@ -99,14 +98,16 @@ class SupremeMonitor:
     def monitor(self):
         while True:
             self.scrape_main_site()
+            time.sleep(0.5)
             self.instock_copy = self.instock.copy()
             for cat in self.pages:
                 for i in cat:
                     for j in cat[i]:
                         self.scrape_item_site(j['name'], j['id'])
+                        time.sleep(0.5)
 
 
 if __name__ == '__main__':
-    discord_webhook_url = ''
+    discord_webhook_url = 'https://discordapp.com/api/webhooks/752642067985072249/6-azDwmlcg2H82Cd4jfM45e_8iP7XljcD-oFABVeuoY-fbzo1gSaHVu8TKjagXBIp2a8'
     test = SupremeMonitor(webhook=discord_webhook_url)
     test.monitor()
