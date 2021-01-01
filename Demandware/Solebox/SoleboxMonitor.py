05b35921a09388e2b085a3ba87c1c0600458b529
Yasser Qureshi <yasser.q@hotmail.com>
2021-01-01 01:23:22 +0000

    Fixed datetime, added initial notification

--- Demandware/Solebox/SoleboxMonitor.py
+++ Demandware/Solebox/SoleboxMonitor.py
@@ -59,12 +59,17 @@ class SoleboxMonitor:
         data["avatar_url"] = CONFIG['AVATAR_URL']
         data["embeds"] = []
         embed = {}
-        embed["title"] = f'{product_item[0]} {product_item[1]}'
-        embed['url'] = f'https://www.solebox.com{product_item[2]}'
+        if product_item == 'initial':
+            embed["description"] = "Thank you for using Yasser's Sneaker Monitors. This message is to let you know " \
+                                   "that everything is working fine! You can find more monitoring solutions at " \
+                                   "https://github.com/yasserqureshi1/Sneaker-Monitors "
+        else:
+            embed["title"] = f'{product_item[0]} {product_item[1]}'
+            embed['url'] = f'https://www.solebox.com{product_item[2]}'
+            embed["thumbnail"] = {'url': product_item[3]}
         embed["color"] = int(CONFIG['COLOUR'])
-        embed["thumbnail"] = {'url': product_item[3]}
         embed["footer"] = {'text': 'Made by Yasser Qureshi'}
-        embed["timestamp"] = str(datetime.datetime.now())
+        embed["timestamp"] = str(datetime.datetime.utcnow())
         data["embeds"].append(embed)
 
         result = requests.post(self.webhook, data=json.dumps(data), headers={"Content-Type": "application/json"})
@@ -90,7 +95,7 @@ class SoleboxMonitor:
 
         print('STARTING MONITOR')
         logging.info(msg='Successfully started monitor')
-
+        self.discord_webhook('initial')
         start = 1
         while True:
             self.scrape_main_site()
