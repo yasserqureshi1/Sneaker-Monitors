7fc90e56de04c0c561b298499b0ea01bd715df9b
Yasser Qureshi <yasser.q@hotmail.com>
2021-05-04 21:55:37 +0100

    Clean up

--- Shopify/ShopifyMonitor.py
+++ Shopify/ShopifyMonitor.py
@@ -1,13 +1,17 @@
+from random_user_agent.params import SoftwareName, HardwareType
+from random_user_agent.user_agent import UserAgent
+
+from fp.fp import FreeProxy
+
 import requests as rq
-import json
-import time
-from datetime import datetime
 import urllib3
+
+from datetime import datetime
+import time
+
+import json
 import logging
 import dotenv
-from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, HardwareType
-from fp.fp import FreeProxy
 
 logging.basicConfig(filename='Shopifylog.log', filemode='a', format='%(asctime)s - %(name)s - %(message)s',
                     level=logging.DEBUG)
@@ -25,7 +29,6 @@ INSTOCK = []
 def check_url(url):
     """
     Checks whether the supplied URL is valid
-    :return: Boolean - True if valid
     """
     return 'products.json' in url
 
@@ -33,9 +36,10 @@ def check_url(url):
 def scrape_site(url, headers, proxy):
     """
     Scrapes the specified Shopify site and adds items to array
-    :return: None
     """
     items = []
+
+    # Makes request to site
     s = rq.Session()
     page = 1
     while True:
@@ -44,63 +48,75 @@ def scrape_site(url, headers, proxy):
         if output == []:
             break
         else:
+            # Stores particular details in array
             for product in output:
-                product_item = [
-                    {'title': product['title'], 'image': product['images'][0]['src'], 'handle': product['handle'],
-                     'variants': product['variants']}]
+                product_item = {
+                    'title': product['title'], 
+                    'image': product['images'][0]['src'], 
+                    'handle': product['handle'],
+                    'variants': product['variants']}
                 items.append(product_item)
-            logging.info(msg='Successfully scraped site')
             page += 1
     
-    time.sleep(float(CONFIG['DELAY']))
+    logging.info(msg='Successfully scraped site')
     s.close()
     return items
 
 
-def checker(handle):
+def checker(item):
     """
     Determines whether the product status has changed
     """
-    for item in INSTOCK:
-        if item == handle:
-            return True
-    return False
+    return item in INSTOCK
 
 
-def discord_webhook(product_item):
+def test_webhook():
+    data = {
+        "username": CONFIG['USERNAME'],
+        "avatar_url": CONFIG['AVATAR_URL'],
+        "embeds": [{
+            "title": "Testing Webhook",
+            "description": "This is just a quick test to ensure the webhook works. Thanks again for using these montiors!",
+            "color": int(CONFIG['COLOUR']),
+            "footer": {'text': 'Made by Yasser'},
+            "timestamp": str(datetime.utcnow())
+        }]
+    }
+
+    result = rq.post(CONFIG['WEBHOOK'], data=json.dumps(data), headers={"Content-Type": "application/json"})
+
+    try:
+        result.raise_for_status()
+    except rq.exceptions.HTTPError as err:
+        logging.error(err)
+    else:
+        print("Payload delivered successfully, code {}.".format(result.status_code))
+        logging.info(msg="Payload delivered successfully, code {}.".format(result.status_code))
+
+
+
+def discord_webhook(title, url, thumbnail, sizes):
     """
     Sends a Discord webhook notification to the specified webhook URL
-    :param product_item: An array containing the product name, product sizes in-stock ans the thumbnail URL
-    :return: None
     """
-    description = ''
-    if product_item[0] == 'initial':
-        description = "Thank you for using Yasser's Sneaker Monitors. This message is to let you know that " \
-                      "everything is working fine! You can find more monitoring solutions at " \
-                      "https://github.com/yasserqureshi1/Sneaker-Monitors "
-    else:
-        fields = []
-        for size in product_item[1][0]:
-            fields.append({'name': size, 'value': 'Available', 'inline': True})
+    fields = []
+    for size in sizes:
+        fields.append({"name": size, "value": "Available", "inline": True})
 
-        link = CONFIG['URL'].replace('.json', '/') + product_item[3]
+    data = {
+        "username": CONFIG['USERNAME'],
+        "avatar_url": CONFIG['AVATAR_URL'],
+        "embeds": [{
+            "title": title,
+            "url": CONFIG['URL'].replace('.json', '/') + url, 
+            "thumbnail": {"url": thumbnail},
+            "fields": fields,
+            "color": int(CONFIG['COLOUR']),
+            "footer": {"text": "Made by Yasser"},
+            "timestamp": str(datetime.utcnow()),
+        }]
+    }
 
-    data = {}
-    data["username"] = CONFIG['USERNAME']
-    data["avatar_url"] = CONFIG['AVATAR_URL']
-    data["embeds"] = []
-    embed = {}
-    if product_item[0] != 'initial':
-        embed["title"] = product_item[0]
-        embed['url'] = link
-        embed["thumbnail"] = {'url': product_item[2]}
-        embed["fields"] = fields
-    else:
-        embed["description"] = description
-    embed["color"] = int(CONFIG['COLOUR'])
-    embed["footer"] = {'text': 'Made by Yasser Qureshi'}
-    embed["timestamp"] = str(datetime.utcnow())
-    data["embeds"].append(embed)
     result = rq.post(CONFIG['WEBHOOK'], data=json.dumps(data), headers={"Content-Type": "application/json"})
 
     try:
@@ -115,82 +131,105 @@ def discord_webhook(product_item):
 def remove_duplicates(mylist):
     """
     Removes duplicate values from a list
-    :param mylist: list
-    :return: list
     """
     return list(set(mylist))
 
 
 def comparitor(product, start):
-    product_item = [product[0]['title'], [], product[0]['image'], product[0]['handle']]
+    product_item = [product['title'], [], product['image'], product['handle']]
+
+    # Collect all available sizes
     available_sizes = []
-    for size in product[0]['variants']:
+    for size in product['variants']:
         if size['available']:
             available_sizes.append(size['title'])
 
     if available_sizes:
-        if checker(product[0]['handle']):
-            pass
-        else:
-            INSTOCK.append(product[0]['handle'])
+        if not checker(product_item):
+            # If product is available but not stored - sends notification and stores
             product_item[1].append(available_sizes)
+            INSTOCK.append(product_item)
+
+            if start == 0:
+                print(product_item)
+                discord_webhook(
+                    title=product['title'],
+                    url=product['handle'],
+                    thumbnail=product['image'],
+                    sizes=available_sizes
+                )
+                logging.info(msg='Successfully sent Discord notification')
+
     else:
-        if checker(product[0]['handle']):
-            INSTOCK.remove(product[0]['handle'])
-    if not product_item[1]:
-        pass
-    else:
-        if start == 0:
-            print(product_item)
-            discord_webhook(product_item)
-            logging.info(msg='Successfully sent Discord notification')
+        if checker(product_item):
+            # If product is not available but stored - removes from store
+            INSTOCK.remove(product_item)
 
 
 def monitor():
     """
     Initiates the monitor
-    :return: None
     """
     print('STARTING MONITOR')
     logging.info(msg='Successfully started monitor')
+
+    # Checks URL
     if not check_url(CONFIG['URL']):
         print('Store URL not in correct format. Please ensure that it is a path pointing to a /products.json file')
         logging.error(msg='Store URL formatting incorrect for: ' + str(CONFIG['URL']))
         return
 
-    discord_webhook(['initial'])
-    start = 1
-    proxy_no = 0
+    # Tests webhook URL
+    test_webhook()
 
+    # Ensures that first scrape does not notify all products
+    start = 1
+
+    # Initialising proxy and headers
+    proxy_no = 0
     proxy_list = CONFIG['PROXY'].split('%')
     proxy = {"http": proxyObject.get()} if proxy_list[0] == "" else {"http": f"http://{proxy_list[proxy_no]}"}
     headers = {'User-Agent': user_agent_rotator.get_random_user_agent()}
+
+    # Collecting all keywords (if any)
     keywords = CONFIG['KEYWORDS'].split('%')
     while True:
         try:
+            # Makes request to site and stores products 
             items = scrape_site(CONFIG['URL'], proxy, headers)
             for product in items:
-                check = False
+
                 if keywords == "":
+                    # If no keywords set, checks whether item status has changed
                     comparitor(product, start)
+
                 else:
+                    # For each keyword, checks whether particular item status has changed
                     for key in keywords:
-                        if key.lower() in product[0]['title'].lower():
-                            check = True
-                            break
-                    if check:
-                        comparitor(product, start)
+                        if key.lower() in product['title'].lower():
+                            comparitor(product, start)
+
+            # Allows changes to be notified
+            start = 0
+
+            # User set delay
+            time.sleep(float(CONFIG['DELAY']))
+
         except Exception as e:
             print(f"Exception found '{e}' - Rotating proxy and user-agent")
             logging.error(e)
+
+            # Rotates headers
             headers = {'User-Agent': user_agent_rotator.get_random_user_agent()}
+            
             if CONFIG['PROXY'] == "":
+                # If no optional proxy set, rotates free proxy
                 proxy = {"http": proxyObject.get()}
+
             else:
+                # If optional proxy set, rotates if there are multiple proxies
                 proxy_no = 0 if proxy_no == (len(proxy_list) - 1) else proxy_no + 1
                 proxy = {"http": f"http://{proxy_list[proxy_no]}"}
-        start = 0
-        time.sleep(float(CONFIG['DELAY']))
 
 
 if __name__ == '__main__':
