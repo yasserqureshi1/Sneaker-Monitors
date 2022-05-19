1a09e273877bb991d94c93b7b2ed37f5f93eaa10
Yasser Qureshi <yasser.q@hotmail.com>
2022-05-19 23:59:46 +0100

    Initial Commit

---
+++ Shopify/config.py
@@ -0,0 +1,33 @@
+# --------------------- WEBHOOK URL ---------------------
+WEBHOOK = ""
+
+# --------------------- SHOPIFY URL ---------------------
+# Insert the url of the Shopify store here. Ensure that it points to /products.json
+# E.G. URL = "https://www.hanon-shop.com/collections/whats-new/products.json"
+URL = "https://www.hanon-shop.com/collections/whats-new/products.json"
+
+# --------------------- FREE PROXY ---------------------
+# This is your location code according to Location-Code-List.md
+ENABLE_FREE_PROXY = False
+FREE_PROXY_LOCATION = "GB"
+
+# --------------------- DELAY ---------------------
+# This is the delay between checks. If you start to get error messages, it's best to increase this.
+# Measured in seconds
+DELAY = 1
+
+# --------------------- OPTIONAL PROXY ---------------------
+# Proxies must follow this format: "<proxy>:<port>" OR "<proxy_username>:<proxy_password>@<proxy_domain>:<port>")
+# If you want to use multiple proxies, please create an array
+# E.G. PROXY = ["proxy1:proxy1port", "proxy2:proxy2port"]
+PROXY = []
+
+# --------------------- OPTIONAL KEYWORDS ---------------------
+# Each keyword should be a separate multiple keywords using "%"
+# E.G. KEYWORDS = ["box","logo"]
+KEYWORDS = []
+
+# --------------------- DISCORD BOT FEATURES ---------------------
+USERNAME = "Nike SNKRS"
+AVATAR_URL = "http://logostories.com/wp-content/uploads/2015/10/image-nike-logo-4.png"
+COLOUR = 16777215
---
+++ Supreme/config.py
@@ -0,0 +1,28 @@
+# --------------------- WEBHOOK URL ---------------------
+WEBHOOK = ""
+
+# --------------------- FREE PROXY ---------------------
+# This is your location code according to Location-Code-List.md
+ENABLE_FREE_PROXY = False
+FREE_PROXY_LOCATION = "GB"
+
+# --------------------- DELAY ---------------------
+# This is the delay between checks. If you start to get error messages, it's best to increase this.
+# Measured in seconds
+DELAY = 1
+
+# --------------------- OPTIONAL PROXY ---------------------
+# Proxies must follow this format: "<proxy>:<port>" OR "<proxy_username>:<proxy_password>@<proxy_domain>:<port>")
+# If you want to use multiple proxies, please create an array
+# E.G. PROXY = ["proxy1:proxy1port", "proxy2:proxy2port"]
+PROXY = []
+
+# --------------------- OPTIONAL KEYWORDS ---------------------
+# Each keyword should be a separate multiple keywords using "%"
+# E.G. KEYWORDS = ["box","logo"]
+KEYWORDS = []
+
+# --------------------- DISCORD BOT FEATURES ---------------------
+USERNAME = "Supreme Monitor"
+AVATAR_URL = "https://lh3.googleusercontent.com/lPUZwKE_VV6_UxQOE_MlXVSYi77LssHVK0T9zZFFERORHI7ZhQXD-WvsMKXu5822NQ"
+COLOUR = 12845619
